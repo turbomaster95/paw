81136172015428ed9352458c1064557693c9610d
Deva Midhun <admin@coder.is-a.dev>
2026-09-17 14:22:29 +0530

    Update dogma.h

--- include/dogma.h
+++ include/dogma.h
@@ -1,9 +1,17 @@
-#ifndef ETC_H
-#define ETC_H
+#ifndef DOGMA_H
+#define DOGMA_H
 
 #include <nu.h>
 
-void dogma_parse(const char* output_file);
+typedef enum {
+    DOGMA_OK = 0,
+    DOGMA_ERR_IO,
+    DOGMA_ERR_SYNTAX,
+    DOGMA_ERR_SEMA,
+    DOGMA_ERR_CODEGEN
+} dogma_status_t;
+
+dogma_status_t dogma_parse(const char* output_file);
 void dogma_ast2file(nu_ast_node_t *node, const char *out_filename);
 
 #endif
